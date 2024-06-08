39446b662dd54d90c4c2509c4c89c31a81c64c64
Shelabh <68127067+shelabh@users.noreply.github.com>
2024-06-09 02:49:39 +0530

    added timeline chart

--- src/pages/Dashboard/Sections/Chart.tsx
+++ src/pages/Dashboard/Sections/Chart.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { DataSet, Timeline, TimelineOptions, TimelineTimeAxisScaleType } from 'vis-timeline/standalone';
+import 'vis-timeline/styles/vis-timeline-graph2d.min.css';
 import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
-
+import './index.css';
 const data = [
   { value: 25, label: 'Bajaj Finserv',number: '25,000',  color: '#6C63FF' },
   { value: 20, label: 'Shriram Finance', number: '20,000', color: '#FA9D17' },
@@ -13,7 +15,54 @@ const size = {
   height: 250,
 };
 const Chart = () => {
-	
+	const timelineRef = useRef<HTMLDivElement | null>(null);
+	const timelineInstance = useRef<Timeline | null>(null);
+      
+	useEffect(() => {
+	  if (timelineRef.current) {
+	    const container = timelineRef.current;
+	    const items = new DataSet([
+	      { id: 1, content: 'Mahindra FD Plan 2', start: '2024-06-01', end: '2026-07-10', className: 'fd-item' },
+	      { id: 2, content: 'Bajaj Finserv FD 1', start: '2024-05-01', end: '2025-04-15', className: 'fd-item' },
+	      { id: 3, content: 'Shriram FD Plan 2', start: '2024-01-01', end: '2025-05-01', className: 'fd-item' },
+	      { id: 4, content: 'Utkarsh FD 1', start: '2024-01-01', end: '2024-08-01', className: 'fd-item' },
+	    ]);
+      
+	    const options: TimelineOptions = {
+	      width: '100%',
+	      height: '250px',
+	      stack: true,
+	      showMajorLabels: true,
+	      showCurrentTime: false,
+	      zoomMin: 1000 * 60 * 60 * 24 * 365 * 3, // three years in milliseconds
+	      zoomMax: 1000 * 60 * 60 * 24 * 365 * 3, // three years in milliseconds
+	      zoomable: false,
+	      start: '2024-01-01',
+	      end: '2026-12-31',
+	      orientation: { axis: 'top' },
+	      margin: { item: 10 },
+	      format: {
+		minorLabels: {
+		  month: 'MMM', // Use the short month format
+		  day: '',
+		},
+		majorLabels: {
+		  year: 'YYYY',
+		},
+	      },
+	      timeAxis: { scale: 'month' as TimelineTimeAxisScaleType, step: 1 }
+	    };
+      
+	    timelineInstance.current = new Timeline(container, items, options);
+	  }
+      
+	  return () => {
+	    if (timelineInstance.current) {
+	      timelineInstance.current.destroy();
+	    }
+	  };
+	}, []);
+      
 	return (
 		<>
 			<div className="flex flex-row gap-5 justify-between ">
@@ -56,9 +105,10 @@ const Chart = () => {
 					<div className='text-lg font-bold'>
 						FD Maturity Timeline
 					</div>
-					<img src="./images/timeline.png" alt="timeline" draggable="false" />
+					<div ref={timelineRef} ></div>
 
 				</div>
+				
 
 			</div>
 		</>
